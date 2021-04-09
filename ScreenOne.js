31ac067e444be5388b53d008a8dde49141e94650
Amilcar Aponte <apontejaj@gmail.com>
2021-04-09 17:12:26 +0100

    Second COmmit

---
+++ ScreenOne.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View, Button } from 'react-native';
+
+export default function ScreenOne({ navigation }) {
+    return (
+        <View>
+            <Text>Hello</Text>
+            <Button
+                onPress={() => {
+                    navigation.navigate('Two')
+                }}
+                title="Go to Screen Two"
+            />
+        </View>
+    );
+}
